3c48fdf1d428609cde70edd24d1fe371ced27d06
60102 <leechenglok@hotmail.com>
2020-08-10 21:06:02 +0800

    commit by cheng lok

--- app/src/main/java/com/example/mobileassignment/MainActivity.java
+++ app/src/main/java/com/example/mobileassignment/MainActivity.java
@@ -8,20 +8,32 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
+    private Button lbutton;
+    private Button pbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button=findViewById(R.id.button2);
-        button.setOnClickListener(new View.OnClickListener(){
+        lbutton=findViewById(R.id.button2);
+        lbutton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
                 openLogin();
             }
             public void openLogin(){
                 Intent intent = new Intent(MainActivity.this,Login.class);
+                startActivity(intent);
+            }
+        });
+
+        pbutton=findViewById(R.id.button4);
+        pbutton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                openRegister();
+            }
+            public void openRegister(){
+                Intent intent = new Intent(MainActivity.this,Register.class);
                 startActivity(intent);
             }
         });
