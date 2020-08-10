598cfd002081d7ba1b79d7c7002110fe8fba4e41
60102 <leechenglok@hotmail.com>
2020-08-10 21:57:25 +0800

    commit by cheng lok

--- app/src/main/java/com/example/mobileassignment/Login.java
+++ app/src/main/java/com/example/mobileassignment/Login.java
@@ -2,14 +2,29 @@ package com.example.mobileassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
-
+    TextView registerLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        registerLink=findViewById(R.id.registerLink);
+        registerLink.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                openRegister();
+            }
+
+            public void openRegister(){
+                Intent intent=new Intent(Login.this,Register.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/mobileassignment/Register.java
+++ app/src/main/java/com/example/mobileassignment/Register.java
@@ -2,13 +2,30 @@ package com.example.mobileassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
+    TextView loginLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        loginLink=findViewById(R.id.loginLink);
+        loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openLogin();
+            }
+
+            public void openLogin(){
+                Intent intent=new Intent(Register.this,Login.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
